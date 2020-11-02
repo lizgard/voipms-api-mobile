0386787a777f6c3eed04ee033c4a4899cc463426
lizgard <edgardo.gordillo@lizgard.com>
2020-11-02 11:54:19 -0600

    update application

---
+++ src/components/App.vue
@@ -0,0 +1,47 @@
+<template>
+    <Page>
+        <ActionBar title="VoIP.ms API Setting"/>
+        <TabView :selectedIndex="selectedIndex" @selectedIndexChange="indexChange">
+            <TabViewItem title="Tab 1">
+                <GridLayout columns="*" rows="*">
+                    <Label class="message" :text="msg" col="0" row="0"/>
+                </GridLayout>
+            </TabViewItem>
+
+            <TabViewItem title="Tab 2">
+                <Label text="Content for Tab 2" />
+            </TabViewItem>
+        </TabView>
+    </Page>
+</template>
+
+<script lang="ts">
+  export default {
+    data() {
+      return {
+        msg: 'El comunismo por el bien de todos'
+      }
+    },
+
+    methods: {
+        indexChange (args): void {
+            let newIndex = args.value;
+            console.log('Current tab index: ' + newIndex);
+        }
+    }
+  }
+</script>
+
+<style scoped>
+    ActionBar {
+        background-color: #53ba82;
+        color: #ffffff;
+    }
+
+    .message {
+        vertical-align: center;
+        text-align: center;
+        font-size: 20;
+        color: #333333;
+    }
+</style>
